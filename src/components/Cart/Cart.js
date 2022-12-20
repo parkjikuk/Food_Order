4f34b16ddb44622f52348758d9b3705c6f581e2e
parkjikuk <paxkk@naver.com>
2022-12-21 01:44:40 +0900

    Add Cart components

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,51 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import classes from './Cart.module.css'
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `${cartCtx.totalAmount}원`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
+
+  const cartItems = (
+  <ul className={classes['cart-items']}>
+    {cartCtx.items.map((item) => (
+      <CartItem 
+        key={item.id}
+        name={item.name}
+        amount={item.amount}
+        price={item.price}
+        onRemove={cartItemRemoveHandler.bind(null, item.id)}
+        onAdd={cartItemAddHandler.bind(null, item)}
+      />
+    ))}
+  </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>총 가격</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>닫기</button>
+        {hasItems && <button className={classes.button}>주문</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
