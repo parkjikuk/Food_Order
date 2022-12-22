1376b560a0b95c15bd0d69e1873705abf8d741f0
parkjikuk <paxkk@naver.com>
2022-12-23 00:23:19 +0900

    feat : Add fetch data and Loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,68 @@
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
+import { useEffect, useState } from 'react';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: '스시',
-    description: '쌀밥에 날생선 등의 해산물이나 달걀 등을 조합한 음식으로, 전통적으로 와사비와 함께 먹는다.',
-    price: 12000,
-  },
-  {
-    id: 'm2',
-    name: '돈까스',
-    description: '이탈리아의 코톨레타를 원형으로 하는 서양의 커틀릿에서 유래한 일본 요리',
-    price: 9500,
-  },
-  {
-    id: 'm3',
-    name: '바베큐 햄버거',
-    description: 'American, raw, meaty',
-    price: 8000,
-  },
-  {
-    id: 'm4',
-    name: '샐러드',
-    description: 'Healthy...and green...',
-    price: 6500,
-  },
-];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />)
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-http-9f2dc-default-rtdb.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error('잠시 후 다시 시도해 주세요')
+      }
+
+      const responseData = await response.json();
+      
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    }
+
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    })
+  }, []);
+
+  if(isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
+
+  const mealsList = meals.map(meal => 
+    <MealItem 
+      id={meal.id} 
+      key={meal.id} 
+      name={meal.name}
+      description={meal.description}
+      price={meal.price} 
+    />)
 
   return (
     <section className={classes.meals}>
